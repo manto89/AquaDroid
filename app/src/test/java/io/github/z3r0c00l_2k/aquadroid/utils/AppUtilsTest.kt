7178b4a72730fc7c16d65117661202a73b060022
manto89 <manto89@gmail.com>
2021-07-19 13:25:24 +0200

    #1 Added AppUtils.differenceInSeconds Added KotlinParametrizedTest annotation (see https://blog.oio.de/2018/11/13/how-to-use-junit-5-methodsource-parameterized-tests-with-kotlin/)

---
+++ app/src/test/java/io/github/z3r0c00l_2k/aquadroid/utils/AppUtilsTest.kt
@@ -0,0 +1,68 @@
+package io.github.z3r0c00l_2k.aquadroid.utils
+
+import KotlinParametrizedTest
+import org.junit.Assert
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AppUtilsTest{
+
+    @Nested
+    @KotlinParametrizedTest
+    @DisplayName("Difference in seconds: ")
+    inner class DifferenceInSecondsTest {
+
+        fun datetimeInSameDay(): Iterable<Array<Any>> {
+            return arrayListOf(
+                arrayOf(
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-18 00:01:00"),
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-18 00:02:00"),
+                    60
+                ),
+                arrayOf(
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-18 00:02:00"),
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-18 00:01:00"),
+                    -60
+                )
+            )
+        }
+        fun datetimeInDifferentDays(): Iterable<Array<Any>>{
+            return arrayListOf(
+
+                arrayOf(
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-18 00:00:00"),
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-17 23:59:00"),
+                    (23 * 3600) + (59 * 60)
+                ),
+                arrayOf(
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-17 23:59:00"),
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-07-18 00:00:00"),
+                    -((23 * 3600) + (59 * 60))
+                )
+            )
+        }
+
+        @ParameterizedTest
+        @MethodSource("datetimeInSameDay")
+        @DisplayName("given valid Dates in same day when called then results are correct")
+        fun sameDayDifferenceInSecondsTest(first: Date, second: Date, expectedDifference: Int) {
+            val difference = AppUtils.differenceInSeconds(first, second)
+            Assert.assertEquals(expectedDifference, difference)
+
+        }
+
+        @ParameterizedTest
+        @MethodSource("datetimeInDifferentDays")
+        @DisplayName("given valid Dates in different days when called then it ignores the date part")
+        fun differentDaysDifferenceInSecondsTest(first: Date, second: Date, expectedDifference: Int) {
+            val difference = AppUtils.differenceInSeconds(first, second)
+            Assert.assertEquals(expectedDifference, difference)
+
+        }
+
+    }
+}
